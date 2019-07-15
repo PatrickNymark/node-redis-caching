4eac6a46f1322bfdb906fef4500ffe459790c52d
Patrick Nymark <pn@ctdenmark.dk>
2019-07-15 13:10:07 +0200

    refactored auth flow

--- client/src/App.js
+++ client/src/App.js
@@ -2,18 +2,11 @@ import React,  { Component } from 'react';
 import { Router, Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 import './style.css'
-
-import { authActions } from './actions';
 import { Navbar } from './components/Navbar';
 import Routes from './components/routing/Routes';
 import { history } from './helpers';
 
-class App extends Component {
-  componentDidMount() {
-    const { dispatch } = this.props;
-    dispatch(authActions.getCurrent());
-  }
-  
+class App extends Component {  
   render() {
     return (
         <Router history={history}>
--- client/src/actions/auth.actions.js
+++ client/src/actions/auth.actions.js
@@ -1,12 +1,10 @@
 import { authConstants } from '../constants';
 import { authService } from '../services';
 import { history, setAuthToken } from '../helpers';
-import jwt_decode from 'jwt-decode';
 
 export const authActions = {
     login,
-    logout,
-    getCurrent
+    logout
 };
 
 function login(email, password) {
@@ -34,33 +32,4 @@ function logout() {
 
     localStorage.removeItem('user');
     return { type: authConstants.LOGOUT };
-}
-
-
-function getCurrent() {
-    return dispatch => {
-        dispatch(request())
-        const token = localStorage.getItem('user');
-        if(token) {
-            const user = jwt_decode(token);
-            // .valueOf to get same date format as jwt exp.
-            const currentDate = Date.now().valueOf() / 1000;
-            // check for expired
-            if(user.exp < currentDate) {
-                localStorage.removeItem('user');
-                return dispatch(failure())
-            }
-            
-            // set axios default header for future requests
-            setAuthToken(token);
-    
-            return dispatch(success(user))
-        }
-        // if not token found, return action with no payload
-        return dispatch(failure());
-    }
-    
-    function request() { return { type: authConstants.USER_REQUEST } }
-    function success(user) { return { type: authConstants.USER_SUCCESS, user } }
-    function failure() { return { type: authConstants.USER_FAILURE } }
 }
