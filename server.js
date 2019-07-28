e0b364db832ad12799c16db0b6dfb944bf0f82b1
Patrick Nymark <pn@ctdenmark.dk>
2019-07-28 13:28:32 +0200

    prepared for production

--- server.js
+++ server.js
@@ -33,6 +33,16 @@ app.use('/api/authors', require('./controllers/author.controller'));
 
 app.use(errorHandler);
 
-const port = 5000 || process.env.PORT;
+// Server static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on ${port}`));
