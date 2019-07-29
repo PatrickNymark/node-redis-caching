582d30933b0d84f84f31c65d61effaa8649dca3c
Patrick Nymark <pn@ctdenmark.dk>
2019-07-29 15:53:50 +0200

    classname fix

--- client/src/components/fragments/Navbar.js
+++ client/src/components/fragments/Navbar.js
@@ -14,34 +14,34 @@ class Navbar extends Component {
     const { isAuthenticated } = this.props.auth;
     console.log(this.props)
     return (
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <Link to="/" class="navbar-brand" href="#">Redis Caching</Link>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <Link to="/" className="navbar-brand" href="#">Redis Caching</Link>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
         </button>
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav mr-auto">
-            {!isAuthenticated && <li class="nav-item active">
-              <Link to="/" class="nav-link">Home<span class="sr-only">(current)</span></Link>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mr-auto">
+            {!isAuthenticated && <li className="nav-item active">
+              <Link to="/" className="nav-link">Home<span className="sr-only">(current)</span></Link>
             </li>}
-            {isAuthenticated && <li class="nav-item">
-              <Link to="/dashboard" class="nav-link">Dashboard</Link>
+            {isAuthenticated && <li className="nav-item">
+              <Link to="/dashboard" className="nav-link">Dashboard</Link>
             </li>}
-            <li class="nav-item">
-              <Link to="/books" class="nav-link">Books</Link>
+            <li className="nav-item">
+              <Link to="/books" className="nav-link">Books</Link>
             </li> 
-            {isAuthenticated && <li class="nav-item">
-              <Link to="/authors" class="nav-link">Authors</Link>
+            {isAuthenticated && <li className="nav-item">
+              <Link to="/authors" className="nav-link">Authors</Link>
             </li>}
-            {!isAuthenticated && <li class="nav-item">
-              <Link to="/login" class="nav-link">Login</Link> 
+            {!isAuthenticated && <li className="nav-item">
+              <Link to="/login" className="nav-link">Login</Link> 
             </li>}
-            {isAuthenticated && <li class="nav-item">
-              <Link onClick={this.handleLogout} to="/" class="nav-link">Logout</Link> 
+            {isAuthenticated && <li className="nav-item">
+              <Link onClick={this.handleLogout} to="/" className="nav-link">Logout</Link> 
             </li>}
-            {!isAuthenticated &&  <li class="nav-item">
-              <Link to="/register" class="nav-link">Register</Link> 
+            {!isAuthenticated &&  <li className="nav-item">
+              <Link to="/register" className="nav-link">Register</Link> 
             </li>}
           </ul>
         </div>
